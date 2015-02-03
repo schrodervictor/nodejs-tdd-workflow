0c28d8b8a5909d55d48e4eb74a392181f4d92a4e
Victor Schröder <schrodervictor@gmail.com>
2015-02-03 17:13:02 +0100

    Writes getSubtotal method (tests OK)

--- shop.js
+++ shop.js
@@ -7,6 +7,14 @@ module.exports.createCart = function() {
         },
         getAll: function() {
             return this.contents;
+        },
+        getSubtotal: function() {
+            return this.contents.reduce(
+                function(sum, elem) {
+                    return sum + elem.price;
+                },
+                0 // Initial value
+            );
         }
     };
 };
