77403315ef5ba948430fdeefae88b0d8fab7d835
Victor Schröder <schrodervictor@gmail.com>
2015-02-03 16:45:22 +0100

    Changes cart module structure to allow multiple instances (tests failing)

--- cart.js
+++ cart.js
@@ -1,9 +1,12 @@
-module.exports= {
-    contents: [],
-    add: function(item) {
-        this.contents.push(item);
-    },
-    getAll: function() {
-        return this.contents;
-    },
+module.exports.createCart = function() {
+
+    return {
+        contents: [],
+        add: function(item) {
+            this.contents.push(item);
+        },
+        getAll: function() {
+            return this.contents;
+        }
+    };
 };
