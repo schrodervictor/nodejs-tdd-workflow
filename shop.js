e37631265d8e94db87e519a18f3425fa6e30e31c
Victor Schröder <schrodervictor@gmail.com>
2015-02-08 00:59:35 +0100

    [GREEN] Modifies getSubtotal method (is now asynchronous)

--- shop.js
+++ shop.js
@@ -17,17 +17,18 @@ module.exports.createCart = function() {
             var quantity = this.contents.length;
             return callback(null, quantity);
         },
-        getSubtotal: function() {
-            return this.contents.reduce(
+        getSubtotal: function(callback) { // callback = function(err, subtotal)
+            var subtotal = this.contents.reduce(
                 function(sum, elem) {
                     return sum + elem.price;
                 },
                 0 // Initial value
             );
+            return callback(null, subtotal);
         },
         empty: function(callback) { // callback = function(err) {}
             this.contents = [];
-            callback();
+            return callback();
         }
     };
 };
