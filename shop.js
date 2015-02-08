77699051fc0d058f0f1ab89c03d8430b53cb32cb
Victor Schröder <schrodervictor@gmail.com>
2015-02-08 17:12:27 +0100

    [REFACTOR] Improves readability in shop.js

--- shop.js
+++ shop.js
@@ -1,7 +1,9 @@
 module.exports.createCart = function() {
 
     return {
+
         contents: [],
+
         add: function(item, callback) { // callback = function(err){}
 
             if(!('price' in item))
@@ -10,7 +12,8 @@ module.exports.createCart = function() {
             this.contents.push(item);
             return callback();
         },
-        remove: function(query, callback) {
+
+        remove: function(query, callback) { // callback = function(err) {}
             this.contents = this.contents.filter(function(item, index, contents) {
                 var match = true;
                 for(var key in query) {
@@ -20,16 +23,19 @@ module.exports.createCart = function() {
                 }
                 return !match;
             });
-            callback();
+            return callback();
         },
+
         getAll: function(callback) { // callback = function(err, cartContents) {}
             var cartContents = this.contents;
             return callback(null, cartContents);
         },
+
         count: function(callback) { // callback = function(err, quantity) {}
             var quantity = this.contents.length;
             return callback(null, quantity);
         },
+
         getSubtotal: function(callback) { // callback = function(err, subtotal)
             var subtotal = this.contents.reduce(
                 function(sum, elem) {
@@ -39,6 +45,7 @@ module.exports.createCart = function() {
             );
             return callback(null, subtotal);
         },
+
         empty: function(callback) { // callback = function(err) {}
             this.contents = [];
             return callback();
