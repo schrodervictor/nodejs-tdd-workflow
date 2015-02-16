83426de73144adc49d067d3c9dfcdcfc85e41f4b
Victor Schröder <schrodervictor@gmail.com>
2015-02-16 15:47:10 +0100

    [GREEN] New assertion: item's match method should return false when item don't match any query's properties

--- test/item.js
+++ test/item.js
@@ -96,6 +96,24 @@ describe('item.js', function() {
             done();
         });
 
+        it('should return false when item don\'t match any query\'s properties', function(done) {
+
+            var item1 = item.create({
+                name: 'Test1',
+                price: 42,
+                description: 'This should be ignored in the comparison'
+            });
+
+            var query = {
+                name: 'Test1',
+                price: 84
+            };
+
+            expect(item1.match(query)).to.be.false;
+
+            done();
+        });
+
     });
 });
 
