c242be6dab9a254ad608977c6a196272882eb331
Victor Schröder <schrodervictor@gmail.com>
2015-02-16 15:29:42 +0100

    [GREEN] Writes the logic for match in items (now an item knows if it matches a simple query)

--- item.js
+++ item.js
@@ -8,7 +8,17 @@ module.exports.create = function(data) {
             else
                 return null;
         },
-        match: function(query) {}
+        match: function(query) {
+
+            var match = true;
+
+            for(var key in query) {
+                match = match && (this.data[key] === query[key]);
+                if(!match) break;
+            }
+
+            return match;
+        }
     };
 
     for (var prop in data) item.data[prop] = data[prop];
