714d7d9756b8108c78481c6fbaf4a3c3f29c8add
Victor Schröder <schrodervictor@gmail.com>
2015-02-22 15:28:54 +0100

    [RED] Changes the structure of item.js to have a named class

--- item.js
+++ item.js
@@ -1,29 +1,33 @@
-module.exports.create = function(data) {
+function Item(data) {
 
-    var item = {
-        data: {},
-        getPrice: function() {
-            if('price' in this.data)
-                return this.data.price;
-            else
-                return null;
-        },
-        match: function(query) {
+    this.data = {};
 
-            var match = true;
-
-            for(var key in query) {
-                // if the item doesn't have the key, it's not a match
-                // because (undefined !== 'something')
-                match = match && (this.data[key] === query[key]);
-                if(!match) break;
-            }
-
-            return match;
-        }
+    this.getPrice = function getPrice() {
+        if('price' in this.data)
+            return this.data.price;
+        else
+            return null;
     };
 
-    for (var prop in data) item.data[prop] = data[prop];
+    this.match = function match(query) {
 
-    return item;
+        var match = true;
+
+        for(var key in query) {
+            // if the item doesn't have the key, it's not a match
+            // because (undefined !== 'something')
+            match = match && (this.data[key] === query[key]);
+            if(!match) break;
+        }
+
+        return match;
+    }
+
+    for (var prop in data) this.data[prop] = data[prop];
+}
+
+
+module.exports.create = function(data) {
+
+    return new Item(data);
 }
